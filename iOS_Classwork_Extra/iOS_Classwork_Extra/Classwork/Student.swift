258647d28fe486e94113c3d6b54c79974c7bbdf2
jar1hamad <jar1hamad@gmail.com>
2020-06-24 20:38:07 +0300

    Done

--- iOS_Classwork_Extra/iOS_Classwork_Extra/Classwork/Student.swift
+++ iOS_Classwork_Extra/iOS_Classwork_Extra/Classwork/Student.swift
@@ -16,7 +16,16 @@ import Foundation
 /// 3. gender: String
 
 //struct ...
-
+struct Student {
+    var name: String
+    var track: String
+    var gender: String
+    func imageName() -> String {
+        if gender == "boy" {
+            return "boyProfileImage"
+        } else {return "girlProfileImage"}
+    }
+}
 
 
 
@@ -50,12 +59,12 @@ import Foundation
 
 // كتبنالك ياها عشان لا تقول (مووعاريف أكتب مصفوففة وااااع 😭)
 // امسح الكود واكتبه بطريقتك المبدعة 😍
-//var students: [Student] =
-//    [
-//        Student(...
-//        Student(...
-//        Student(...
-//    ]
+var students: [Student] =
+    [
+        Student(name: "ريم خالد", track: "iOS", gender: "girl"),
+        Student(name: "صالح شهاب", track: "Web", gender: "boy"),
+        Student(name: "محمد علي", track: "Android", gender: "boy")
+    ]
 
 
 
